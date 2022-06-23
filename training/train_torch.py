2a6bd8019a3df8a5bdda7b8d3601dca5f87e86f7
ByrdOfAFeather <matthew_a_byrd@outlook.com>
2022-06-22 20:42:47 -0400

    Balanced

--- training/train_torch.py
+++ training/train_torch.py
@@ -3,7 +3,9 @@ import torch.nn
 from scipy.special import softmax
 from datasets import load_metric
 from transformers import TrainingArguments, Trainer, AutoTokenizer, AutoModel, AutoConfig, \
-	AutoModelForSequenceClassification
+    AutoModelForSequenceClassification
+
+from training.trainers import BalancedWeightUpdateTrainer
 from utils import config
 from modeling.models import BertClassifier, DebertClassifier
 from utils.eval_model import compute_metric
@@ -13,42 +15,45 @@ tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", model_max_length=
 
 
 def tokenize(x):
-	return tokenizer(x["discourse_text"], padding="max_length", truncation=True)
+    return tokenizer(x["discourse_text"], padding="max_length", truncation=True)
 
 
 def train():
-	# configer = AutoConfig.from_pretrained("/home/byrdofafeather/ByrdOfAFeather/SSGOGETA/training/test_trainer/checkpoint-29000")
-	# model_container = AutoModelForSequenceClassification.from_config(configer)
-	training_args = TrainingArguments(output_dir="test_trainer", per_device_train_batch_size=5,
-	                                  evaluation_strategy="steps", num_train_epochs=3, seed=225530,
-	                                  run_name="bert_finetune_discourse_text_and_type", save_steps=1500)
-	dataset = config.load_train_val_huggingface()
-	tokenized = dataset.map(tokenize, batched=True)
-	train_set = tokenized["train"].shuffle(seed=225530)
-	val_set = tokenized["test"].shuffle(seed=225530)
-	for i in [0.1, 0.2, 0.3, 0.4]:
-		model_container = BertClassifier(dropout=i)
-		trainer = Trainer(
-			model=model_container.underlying_model,
-			train_dataset=train_set,
-			eval_dataset=val_set,
-			compute_metrics=compute_metric,
-			args=training_args,
-		)
-		trainer.train()
-		print(trainer.evaluate(val_set))
-		print("here")
+    # configer = AutoConfig.from_pretrained("/home/byrdofafeather/ByrdOfAFeather/SSGOGETA/training/test_trainer/checkpoint-29000")
+    # model_container = AutoModelForSequenceClassification.from_config(configer)
+    training_args = TrainingArguments(output_dir="test_trainer", per_device_train_batch_size=5,
+                                      evaluation_strategy="steps", num_train_epochs=3, seed=225530,
+                                      run_name="bert_finetune_discourse_text_and_type", save_steps=1500,
+                                      auto_find_batch_size=True, report_to="tensorboard")
+    dataset = config.load_train_val_huggingface()
+    tokenized = dataset.map(tokenize, batched=True)
+    train_set = tokenized["train"].shuffle(seed=225530)
+    val_set = tokenized["test"].shuffle(seed=225530)
+    for i in [0.1, 0.2, 0.3, 0.4]:
+        model_container = BertClassifier(dropout=i)
+        trainer = BalancedWeightUpdateTrainer(
+            weights=[0.5698354413164695, 0.25373997008023935, 0.17642458860329116],
+            model=model_container.underlying_model,
+            train_dataset=train_set,
+            eval_dataset=val_set,
+            compute_metrics=compute_metric,
+            args=training_args,
+        )
+        trainer.train()
+        print(trainer.evaluate(val_set))
+        print("here")
 
 
 def eval_baseline():
-	train_set, val_set = config.load_bert_train_val()
-	ineffective_label, effective_label, adequate_label = config.get_encoder().transform(["Ineffective", "Effective", "Adequate"])
-	print(ineffective_label, effective_label, adequate_label)
-	prob_adequate = len(train_set[train_set["label"] == adequate_label]) / train_set.shape[0]
-	prob_effective = len(train_set[train_set["label"] == effective_label]) / train_set.shape[0]
-	prob_ineffective = len(train_set[train_set["label"] == ineffective_label]) / train_set.shape[0]
-	preds = [(prob_adequate, prob_effective, prob_ineffective) for _ in val_set.index]
-	print(compute_metric((preds, val_set["label"])))
+    train_set, val_set = config.load_bert_train_val()
+    ineffective_label, effective_label, adequate_label = config.get_encoder().transform(
+        ["Ineffective", "Effective", "Adequate"])
+    print(ineffective_label, effective_label, adequate_label)
+    prob_adequate = len(train_set[train_set["label"] == adequate_label]) / train_set.shape[0]
+    prob_effective = len(train_set[train_set["label"] == effective_label]) / train_set.shape[0]
+    prob_ineffective = len(train_set[train_set["label"] == ineffective_label]) / train_set.shape[0]
+    preds = [(prob_adequate, prob_effective, prob_ineffective) for _ in val_set.index]
+    print(compute_metric((preds, val_set["label"])))
 
 
 train()
---
+++ training/trainers.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+import torch
+from transformers import Trainer
+
+
+class BalancedWeightUpdateTrainer(Trainer):
+    def __init__(self, weights, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.weights = weights
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        outputs = model(**inputs)
+        logits = outputs.get('logits')
+        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(self.weights))
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
